de25a7b1aa73dd22ee922dafe026e705511c37fc
gauss2302 <shilov6865@gmail.com>
2024-12-17 17:59:05 +0500

    Finished usecases for places

--- internal/handler/http/place_handler.go
+++ internal/handler/http/place_handler.go
@@ -1,1 +1,33 @@
 package http
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"ketu_backend_monolith_v1/internal/domain"
+	"ketu_backend_monolith_v1/internal/service"
+)
+
+type PlaceHandler struct {
+	placeUsecase *service.PlaceUseCase
+}
+
+func NewPlaceHandler(placeUsecase *service.PlaceUseCase) *PlaceHandler {
+	return &PlaceHandler{
+		placeUsecase: placeUsecase,
+	}
+}
+
+func (h *PlaceHandler) Create(c *fiber.Ctx) error {
+	var place domain.Place
+	if err := c.BodyParser(&place); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse JSON",
+		})
+	}
+
+	if err := h.placeUsecase.CreatePlace(c.Context(), &place); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "cannot create place",
+		})
+	}
+	return c.Status(fiber.StatusCreated).JSON(place)
+}
--- internal/service/place.go
+++ internal/service/place.go
@@ -1,6 +1,10 @@
 package service
 
-import "ketu_backend_monolith_v1/internal/repository"
+import (
+	"context"
+	"ketu_backend_monolith_v1/internal/domain"
+	"ketu_backend_monolith_v1/internal/repository"
+)
 
 type PlaceUseCase struct {
 	placeRepo repository.PlaceRepository
@@ -8,4 +12,32 @@ type PlaceUseCase struct {
 
 func NewPlaceUsecase(placeRepo repository.PlaceRepository) *PlaceUseCase {
 	return &PlaceUseCase{placeRepo: placeRepo}
+}
+
+func (p *PlaceUseCase) CreatePlace(ctx context.Context, place *domain.Place) error {
+	return p.placeRepo.Create(ctx, place)
+}
+
+func (p *PlaceUseCase) GetPlaceByID(ctx context.Context, id uint) (*domain.Place, error) {
+	return p.placeRepo.GetByID(ctx, id)
+}
+
+func (p *PlaceUseCase) UpdatePlaceByID(ctx context.Context, id uint, place *domain.Place) error {
+	return p.placeRepo.UpdateByID(ctx, id, place)
+}
+
+func (p *PlaceUseCase) DeletePlaceByID(ctx context.Context, id uint) error {
+	return p.placeRepo.DeletePlaceByID(ctx, id)
+}
+
+func (p *PlaceUseCase) GetAllPlaces(ctx context.Context) ([]domain.Place, error) {
+	return p.placeRepo.GetAll(ctx)
+}
+
+func (p *PlaceUseCase) GetPlaceByName(ctx context.Context, name string) (*domain.Place, error) {
+	return p.placeRepo.GetbyName(ctx, name)
+}
+
+func (p *PlaceUseCase) GetPlaceByLocation(ctx context.Context, location string) (*domain.Place, error) {
+	return p.placeRepo.GetbyLocation(ctx, location)
 }
